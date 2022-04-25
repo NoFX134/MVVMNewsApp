4d750f70b4b7d789ff27c24dbd319e160d9aeca6
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-04-25 14:15:05 +0700

    add Fragments

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -2,11 +2,9 @@ package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
-import com.androiddevs.mvvmnewsapp.databinding.ActivityNewsBinding
+
 
 
 class NewsActivity : AppCompatActivity() {
