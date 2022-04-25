70c893b21a01ad04e77326a9b925504631199a6e
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-04-25 23:21:44 +0700

    add Fragments

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY ="73cbbd93a70f4bb48be9f886e46577b7"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
