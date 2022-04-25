145bfa715790aac2a3b29ed13aebe9c5a4835c28
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-04-25 23:26:14 +0700

    add Fragments

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -3,5 +3,5 @@ package com.androiddevs.mvvmnewsapp.ui.fragments
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class BreakingNewsFragment: Fragment() {
+class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
 }
