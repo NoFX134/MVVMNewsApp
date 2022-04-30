fb7a4c1a60a74f257c846f31401cc1f7d01ea146
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-04-30 18:16:36 +0700

    Coorect Time

--- app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsAdapter.kt
@@ -10,7 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.databinding.ItemArticlePreviewBinding
 import com.androiddevs.mvvmnewsapp.models.Article
+import com.androiddevs.mvvmnewsapp.utils.Constants.Companion.TIME_PATTERN
 import com.bumptech.glide.Glide
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
@@ -19,12 +23,14 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var binding: ItemArticlePreviewBinding = ItemArticlePreviewBinding.bind(itemView)
+
         fun bind(article: Article) {
             Glide.with(itemView).load(article.urlToImage).into(binding.ivArticleImage)
             binding.tvSource.text = article.source.name
             binding.tvTitle.text = article.title
             binding.tvDescription.text = article.description
-            binding.tvPublishedAt.text = article.publishedAt
+            binding.tvPublishedAt.text = ZonedDateTime.parse(article.publishedAt)
+                .format(DateTimeFormatter.ofPattern(TIME_PATTERN, Locale("ru")))
             itemView.setOnClickListener {
                 onItemClickListener?.let { it(article) }
             }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -1,8 +1,9 @@
 package com.androiddevs.mvvmnewsapp.utils
 
 class Constants {
-    companion object{
-        const val API_KEY ="73cbbd93a70f4bb48be9f886e46577b7"
+    companion object {
+        const val API_KEY = "73cbbd93a70f4bb48be9f886e46577b7"
         const val BASE_URL = "https://newsapi.org/"
+        const val TIME_PATTERN = "dd MMMM yyyy hh:mm"
     }
 }
