ec679b83fc1f7dd68addc508f245995ab51d4a13
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-05-19 20:06:08 +0700

    Delete symbols <li> from description

--- app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsPagingAdapter.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsPagingAdapter.kt
@@ -29,7 +29,7 @@ class NewsPagingAdapter :
             Glide.with(itemView).load(article?.urlToImage).into(binding.ivArticleImage)
             binding.tvSource.text = article?.source?.name
             binding.tvTitle.text = article?.title
-            binding.tvDescription.text = article?.upper()
+            binding.tvDescription.text = article?.clearDescription()
             binding.tvPublishedAt.text = ZonedDateTime.parse(article?.publishedAt)
                 .format(DateTimeFormatter.ofPattern(TIME_PATTERN, Locale("ru")))
             itemView.setOnClickListener {
